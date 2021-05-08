8c48044039f789277228c7a6e94a1188f9da18bf
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2021-05-08 11:19:11 -0300

    Adicionando movimentação do player

--- Player.js
+++ Player.js
@@ -1,6 +1,6 @@
-function Player(sprite, x, y,classename,  h, w) {
+function Player(sprite, x, y, classename, h, w) {
 
-    GameObject.call(this, sprite, x,y,"player", h,w);
+    GameObject.call(this, sprite, x, y, "player", h, w);
 
 }
 
@@ -9,4 +9,25 @@ Player.prototype = Object.create(GameObject.prototype);
 
 Player.prototype.update = function () {
 
+    //movimento
+    if (se.teclado.CIMA) {
+
+        if (this.y > 50)
+            this.y -=3;
+
+    } else if (se.teclado.BAIXO) {
+        if (this.y < canvas.height - 100)
+            this.y += 3;
+    }
+
+    if (se.teclado.DIREITA) {
+        this.x +=3;
+    } else if (se.teclado.ESQUERDA) {
+        this.x -=3;
+    }
+
+    //laser
+    if (se.teclado.ESPACO) {
+
+    }
 };
